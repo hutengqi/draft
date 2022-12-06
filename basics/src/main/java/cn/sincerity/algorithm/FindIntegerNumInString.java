64d55653d62732546fce5cfffeeed742a19c0ecc
Ht7 <18838292213@163.com>
2022-12-06 17:17:52 +0800

    add remark for count of different integers in string.

--- basics/src/main/java/cn/sincerity/algorithm/FindIntegerNumInString.java
+++ basics/src/main/java/cn/sincerity/algorithm/FindIntegerNumInString.java
@@ -14,7 +14,7 @@ public class FindIntegerNumInString {
 
     public static void main(String[] args) {
         String str = "035985750011523523129774573439111590559325a1554234973";
-        int count = getCountOfInteger(str);
+        int count = numDifferentIntegers(str);
         System.out.println(count);
     }
 
@@ -24,24 +24,32 @@ public class FindIntegerNumInString {
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
-    public int numDifferentIntegers(String word) {
-        Set<String> set = new HashSet<String>();
-        int n = word.length(), p1 = 0, p2;
+    public static int numDifferentIntegers(String word) {
+        Set<String> set = new HashSet<>();
+        //定义变量 p1: 序列起始索引 p2: 序列终止索引
+        int len = word.length(), p1 = 0, p2;
+        //循环寻找数字序列
         while (true) {
-            while (p1 < n && !Character.isDigit(word.charAt(p1))) {
+            // 找到此轮数字字符序列的起始索引
+            while (p1 < len && !Character.isDigit(word.charAt(p1)))
                 p1++;
-            }
-            if (p1 == n) {
+
+            // 说明此次剩余的字符都不是数字或已经检查完毕
+            if (p1 == len)
                 break;
-            }
+
+            // 找到当前数字序列的终止索引 p2
             p2 = p1;
-            while (p2 < n && Character.isDigit(word.charAt(p2))) {
+            while (p2 < len && Character.isDigit(word.charAt(p2)))
                 p2++;
-            }
-            while (p2 - p1 > 1 && word.charAt(p1) == '0') { // 去除前导 0
+
+            // 舍弃数字中的前导0
+            while (p2 - p1 > 1 && word.charAt(p1) == '0')
                 p1++;
-            }
+
+            // 将处理后的子序列加入 set
             set.add(word.substring(p1, p2));
+            // 重置起始索引 p1
             p1 = p2;
         }
         return set.size();
